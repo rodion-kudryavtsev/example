c1fb3207b5a2b61d06edd1c16e591f1329d5bcd9
rodion-kudryavtsev <43132514+rodion-kudryavtsev@users.noreply.github.com>
2022-03-09 11:08:40 +0300

    Add files via upload

---
+++ response.php
@@ -0,0 +1,31 @@
+<?php
+namespace WD\Pro\Sms;
+
+class Response{
+
+	protected $mResponse;
+	protected $bSuccess;
+	protected $strMessageId;
+
+	public function __construct($mResponse){
+		$this->mResponse = $mResponse;
+		$this->bSuccess = false;
+		$this->strMessageId = '';
+	}
+
+	public function setSuccess(bool $bSuccess){
+		$this->bSuccess = $bSuccess;
+	}
+
+	public function setMessageId(string $strMessageId){
+		$this->strMessageId = $strMessageId;
+	}
+
+	public function getResponse(){
+		return $this->mResponse;
+	}
+
+	public function getSuccess(){
+		return $this->bSuccess;
+	}
+}
